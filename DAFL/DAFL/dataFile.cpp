f9406bc9b66f54dadb8fcd48b1359cfb7fc3d026
Casey Griggs <kcgymgrl@gmail.com>
2019-10-04 14:13:54 -0500

    Setting up project

---
+++ DAFL/DAFL/dataFile.cpp
@@ -0,0 +1,40 @@
+#include "dataFile.h"
+
+void dataFile::createFile(char*, unsigned int)
+{
+}
+
+void dataFile::openFile(char* fn)
+{
+
+}
+
+void dataFile::closeFile()
+{
+
+}
+
+void dataFile::putRecord(int, const void*)
+{
+
+}
+
+void dataFile::getRecord(int, const void*)
+{
+
+}
+
+int dataFile::recordCount()
+{
+
+}
+
+void dataFile::updateRecordCount(int)
+{
+
+}
+
+int dataFile::fileStatus()
+{
+
+}
